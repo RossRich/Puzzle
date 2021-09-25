d10f05ec22ff6fc8aec5bb15dd6baea1057abdf8
Ross Rich <RossRich@bk.ru>
2021-09-25 19:06:13 +0000

    add: opencv repo add: base for ball tracking -> BallTracking.cpp fix:  launch file for spawn multiple uav

---
+++ src/PxSitl/scripts/feature_matching_stream.py
@@ -0,0 +1,136 @@
+import cv2
+import numpy as np
+# import pyrealsense2 as rs
+import time
+
+QUERY_IMG_SIZE = (640, 480)
+
+
+def feature_matching_detect_obj(query_img, train_img):
+    MIN_MATCH_COUNT = 30
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+
+    query_img = cv2.resize(query_img, dsize=QUERY_IMG_SIZE)
+    sift = cv2.xfeatures2d.SIFT_create()
+    sift_kp1, sift_des1 = sift.detectAndCompute(query_img, None)
+    sift_kp2, sift_des2 = sift.detectAndCompute(train_img, None)
+    if (sift_des1 is not None and len(sift_des1) != 0) and \
+            (sift_des2 is not None and len(sift_des2) != 0):
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        sift_matches = flann.knnMatch(sift_des1, sift_des2, k=2)
+
+        good = []
+        for m, n in sift_matches:
+            if m.distance < 0.6 * n.distance:
+                good.append(m)
+
+        if len(good) > MIN_MATCH_COUNT:
+            print('Good match points:', len(good))
+            src_pts = np.float32([sift_kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([sift_kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+            homo_matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10.0)
+            homo_res_polyline_img = train_img
+            matches_mask = None
+            if homo_matrix is not None:
+                matches_mask = mask.ravel().tolist()
+                h, w, d = query_img.shape
+                pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(pts, homo_matrix)
+                homo_res_polyline_img = cv2.polylines(train_img, [np.int32(dst)],
+                                                      True, 255, 3, cv2.LINE_AA)
+        else:
+            print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+            matches_mask = None
+            homo_res_polyline_img = train_img
+
+        draw_params = dict(matchColor=(0, 255, 0),
+                           singlePointColor=None,
+                           matchesMask=matches_mask,
+                           flags=2)
+        homo_res_img = cv2.drawMatches(query_img, sift_kp1, homo_res_polyline_img,
+                                       sift_kp2, good, None, **draw_params)
+    else:
+        homo_res_img = cv2.drawMatches(query_img, sift_kp1, train_img, sift_kp2, None,
+                                       None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    return homo_res_img
+
+
+DEVICE_DEPTH_IMG_SIZE = (1280, 720)
+DEVICE_RGB_IMG_SIZE = (1280, 720)
+
+# pipeline = rs.pipeline()
+# config = rs.config()
+# pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+# pipeline_profile = config.resolve(pipeline_wrapper)
+# device = pipeline_profile.get_device()
+# device_product_line = str(device.get_info(rs.camera_info.product_line))
+# config.enable_stream(rs.stream.depth, DEVICE_DEPTH_IMG_SIZE[0], DEVICE_DEPTH_IMG_SIZE[1], rs.format.z16, 30)
+# config.enable_stream(rs.stream.color, DEVICE_RGB_IMG_SIZE[0], DEVICE_RGB_IMG_SIZE[1], rs.format.bgr8, 30)
+
+# pipeline.start(config)
+# print('Device:', device_product_line)
+print('Start streaming ...')
+
+cap = cv2.VideoCapture(0)
+if(cap.isOpened() == False):
+    print("Unable to read camera feed")
+
+query_img = cv2.imread('/workspaces/Puzzle/src/PxSitl/data/IMG_20210920_201017.jpg', cv2.IMREAD_COLOR)
+try:
+    while True:
+        frames = cap.read()
+        # depth_frame = frames.get_depth_frame()
+        # color_frame = frames.get_color_frame()
+        # if not depth_frame or not color_frame:
+            # continue
+
+        # depth_image = np.asanyarray(depth_frame.get_data())
+        # color_image = np.asanyarray(color_frame.get_data())
+
+        t = time.time()
+        detect_res_img = feature_matching_detect_obj(query_img, frames)
+        t = time.time() - t
+        print('Inference time: {:.3f}'.format(t))
+
+        cv2.namedWindow('Detector', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Detector', detect_res_img)
+        ch = cv2.waitKey(1)
+        if ch & 0xFF == ord('q') or ch == 27:
+            break
+
+finally:
+    print('Stop streaming ...')
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Недостатки алгоритма обнаружения объекта (куба) на ключевых точках с использованием гомографии:
+# 1. Предназначен больше для плоских объектов (тетради, журналы, книги) -> попробовать на кубе
+# 2. Неробастный метод - т.е. он пороговый, зависит от условий освещения,
+#    и в целом много ложных срабатываний -> для куба подобрать пороги, придумать эвристики
+#    для отсева ложных срабатываний (использовать несколько кадров, ...)
+# 3. Не может детектить одновременно несколько объектов (только один) -> ?
+# 4. Зависит от позы объекта на шаблоне т.е. для детекции куба надо несколько шаблонов с разными ракурсами,
+#    а потом это постпроцессить -> постпроцессинг
+# 5. Из п.4 следует, что алгоритм нужно прогонять для каждого кадра несколько раз (по количеству шаблонов),
+#    вычислительные затраты возрастут в разы (при том что алгоритм сам по себе вычислительно затратный -
+#    извлечение фич, knn, гомография) -> можно попробовать что-то оптимизировать
+
+
+# https://docs.opencv.org/4.5.1/d7/dff/tutorial_feature_homography.html
+# https://github.com/mtszkw/matching/blob/master/matching/run_matching.py
+# http://amroamroamro.github.io/mexopencv/opencv/feature_homography_demo.html
+# https://github.com/GigaFlopsis/image_pose_estimation/tree/master/src
+# https://docs.opencv.org/master/dc/d2c/tutorial_real_time_pose.html
+# https://docs.opencv.org/master/d7/d53/tutorial_py_pose.html
+# 
+# solvePnP, solvePnPRansac, projectPoints, findFundamentalMat
+#
+# https://overcoder.net/q/77430/%D0%BF%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BA%D0%B0%D0%BC%D0%B5%D1%80%D1%8B-%D0%B2-%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%BE%D0%B9-%D0%BA%D0%BE%D0%BE%D1%80%D0%B4%D0%B8%D0%BD%D0%B0%D1%82%D0%B5-%D0%BE%D1%82-cv-solvepnp
+# https://learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
+#
+
---
+++ src/PxSitl/src/vision/BallTracking.cpp
@@ -0,0 +1,201 @@
+#include <deque>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <ros/ros.h>
+
+using cv::Mat;
+using std::cout;
+using std::endl;
+
+cv::Vec<cv::Scalar_<uint8_t>, 2> findThreshold(cv::VideoCapture &cap,
+                                               cv::FileStorage &settings) {
+
+  const char *winName = "Do mask";
+
+  Mat frame;
+  cv::namedWindow(winName, cv::WINDOW_AUTOSIZE | cv::WINDOW_KEEPRATIO |
+                               cv::WINDOW_GUI_NORMAL);
+
+  uint16_t imWidht = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+  uint16_t imHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+  cv::Size2i rectSize(70, 70);
+  cv::Point2i rectPos(cvRound(imWidht / 2.0) - cvRound(rectSize.width / 2.0),
+                      cvRound(imHeight / 2.0) - cvRound(rectSize.height / 2.0));
+
+  char c = ' ';
+  cv::Scalar_<uint8_t> max;
+  cv::Scalar_<uint8_t> min = {255, 255, 255};
+
+  bool isScan = false;
+
+  while (c != 'q') {
+    cap >> frame;
+
+    Mat roi(frame, cv::Rect2i(rectPos, rectSize));
+
+    cv::cvtColor(roi, roi, cv::COLOR_BGR2HSV);
+
+    switch (c) {
+    case 's':
+      isScan = !isScan;
+      cout << "Started to scan a color..." << endl;
+      break;
+    case 'r':
+      cout << "Restarted a colors" << endl;
+      max = max.all(0);
+      min = min.all(255);
+      break;
+    default:
+      break;
+    }
+
+    if (isScan) {
+      for (size_t i = 0; i < roi.rows; i++) {
+        for (size_t j = 0; j < roi.cols; j++) {
+          cv::Vec3b row = roi.at<cv::Vec3b>(i, j);
+          if (max[0] < row[0]) {
+            max[0] = row[0];
+          }
+
+          if (row[0] < min[0]) {
+            min[0] = row[0];
+          }
+
+          if (max[1] < row[1]) {
+            max[1] = row[1];
+          }
+
+          if (row[1] < min[1]) {
+            min[1] = row[1];
+          }
+
+          if (max[2] < row[2]) {
+            max[2] = row[2];
+          }
+
+          if (row[2] < min[2]) {
+            min[2] = row[2];
+          }
+        }
+      }
+    }
+
+    cv::rectangle(frame, cv::Rect2i(rectPos, rectSize), max, 3, cv::LINE_8);
+    cv::imshow(winName, frame);
+    c = cv::waitKey(5);
+  }
+
+  settings << "threshold"
+           << "{";
+  settings << "min" << min;
+  settings << "max" << max;
+  settings << "}";
+
+  cv::destroyWindow(winName);
+
+  return {min, max};
+}
+
+int main(int argc, char *argv[]) {
+  const char *confFile = "/workspaces/Puzzle/src/PxSitl/data/config.yaml";
+
+  cv::FileStorage conf;
+  cv::Vec<cv::Scalar_<uint8_t>, 2> threshold;
+  bool isDataAvalable = false;
+
+  try {
+    if (conf.open(confFile, cv::FileStorage::READ)) {
+      cv::FileNode thresholdNode = conf["threshold"];
+      if (!thresholdNode.empty()) {
+        int i = 0;
+        for (auto t : thresholdNode)
+          thresholdNode[t.name()] >> threshold[i++];
+
+        isDataAvalable = true;
+      }
+    }
+
+    conf.release();
+  } catch (cv::Exception e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  const char *mainWin = "Main";
+
+  // ros::init(argc, argv, "vison");
+
+  cv::VideoCapture vc;
+  vc.open(0);
+
+  if (!vc.isOpened()) {
+    ROS_ERROR("No video stream");
+    return -1;
+  }
+
+  uint16_t imWidth = vc.get(cv::CAP_PROP_FRAME_WIDTH) / 2;
+  uint16_t imHeight = vc.get(cv::CAP_PROP_FRAME_HEIGHT) / 2;
+
+  ROS_INFO("Width: %i, height: %i", imWidth, imHeight);
+
+  if (!isDataAvalable) {
+    if (!conf.open(confFile, cv::FileStorage::WRITE)) {
+      std::cerr << "Faild open file in " << confFile << endl;
+      vc.release();
+      return -1;
+    }
+    threshold = findThreshold(vc, conf);
+  }
+
+  cv::namedWindow(mainWin, cv::WINDOW_AUTOSIZE);
+
+  Mat frame;
+
+  Mat view(cv::Size2i(imWidth * 2, imHeight), CV_8UC3, cv::Scalar::all(255));
+  Mat view1 = view(cv::Rect2i(0, 0, imWidth, imHeight));
+  Mat view2 = view(cv::Rect2i(imWidth, 0, imWidth, imHeight));
+
+  std::vector<std::vector<cv::Point2i>> cnt;
+
+  while (true) {
+    vc >> frame;
+
+    if (frame.empty())
+      break;
+
+    cv::resize(frame, frame, cv::Size2i(imWidth, imHeight));
+
+    Mat mask;
+    GaussianBlur(frame, mask, cv::Size2i(11, 11), 0);
+    cv::cvtColor(frame, mask, cv::COLOR_BGR2HSV);
+    cv::inRange(
+        mask, cv::Scalar(threshold[0][0], threshold[0][1], threshold[0][2]),
+        cv::Scalar(threshold[1][0], threshold[1][1], threshold[1][2]), mask);
+
+    cv::erode(mask, mask, Mat(), cv::Point2i(), 5);
+    cv::dilate(mask, mask, Mat(), cv::Point2i(), 6);
+
+    cv::findContours(mask.clone(), cnt, cv::RETR_EXTERNAL,
+                     cv::CHAIN_APPROX_SIMPLE);
+
+    cv::drawContours(frame, cnt, -1, cv::Scalar::all(255));
+
+    cout << "Contours count " << cnt.size() << endl;
+
+    cv::cvtColor(mask, mask, cv::COLOR_GRAY2BGR);
+    mask.copyTo(view2);
+    frame.copyTo(view1);
+    
+    cv::imshow(mainWin, view);
+
+    char c = cv::waitKey(1);
+
+    if (c == 'q')
+      break;
+  }
+
+  conf.release();
+  vc.release();
+  cv::destroyAllWindows();
+  return 0;
+}
