1e6dae22dfc23335244cba8fd4d17ee136f13b45
Ross Rich <RossRich@bk.ru>
2022-03-05 13:57:30 +0000

    fix: small changes for correct takeoff

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /usr/share/gazebo/setup.bash
+source /workspaces/Puzzle/src/PX4-Autopilot/Tools/setup_gazebo.bash /workspaces/Puzzle/src/PX4-Autopilot /workspaces/Puzzle/src/PX4-Autopilot/build/px4_sitl_default
+source /opt/ros/noetic/setup.bash
