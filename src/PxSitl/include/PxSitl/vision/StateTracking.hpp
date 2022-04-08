e2709c943450ff6b5a1f7045ca81688a0734f06a
Ross Rich <RossRich@bk.ru>
2022-04-08 16:30:48 +0000

    refactor: add conceptOne

--- src/PxSitl/include/PxSitl/vision/StateTracking.hpp
+++ src/PxSitl/include/PxSitl/vision/StateTracking.hpp
@@ -1,34 +1,36 @@
 #if !defined(_VISION_STATE_TRACKING_H_)
 #define _VISION_STATE_TRACKING_H_
 
-#include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_ros/buffer.h>
-#include <image_geometry/pinhole_camera_model.h>
-#include <image_transport/image_transport.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/TransformStamped.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <std_srvs/Empty.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/image_transport.h>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <std_srvs/Empty.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/Marker.h>
 
-#include "State.hpp"
-#include "RosVH.hpp"
-#include "VideoHandler.hpp"
-#include "BallTrackingRos.hpp"
 #include "BallTracking.hpp"
-#include "utils/thresholdtype.hpp"
+#include "BallTrackingRos.hpp"
+#include "RosVH.hpp"
+#include "State.hpp"
+#include "VideoHandler.hpp"
 #include "utils/Utils.hpp"
+#include "utils/thresholdtype.hpp"
 
+using geometry_msgs::Pose;
+using geometry_msgs::PosePtr;
+using geometry_msgs::TransformStamped;
+using image_geometry::PinholeCameraModel;
 using sensor_msgs::CameraInfo;
 using sensor_msgs::CameraInfoConstPtr;
 using visualization_msgs::Marker;
 using visualization_msgs::MarkerConstPtr;
-using geometry_msgs::TransformStamped;
-using image_geometry::PinholeCameraModel;
 
 class StateTracking : public State {
 private:
@@ -38,42 +40,53 @@ private:
 
   BallTracking *_bt = nullptr;
   RosVH *_vh = nullptr;
-  
+
   std::string _confFile = "";
   std::string _cameraInfoTopic = "/camera/color/camera_info";
-  std::list<cv::Point3d> _ballTragectory;
-  std::list<geometry_msgs::Point> _ballPredictedTraj;
-  std::list<std::list<geometry_msgs::Point>> _ballPredictedTrajs;
+  std::list<geometry_msgs::Point> _objRealLine;
+  std::list<geometry_msgs::Point> _objPredictedLine;
 
-  ros::Time _timer;
-  ros::Time _lastDetection;
-  ros::Time startTime;
-  ros::Time resetTimer;
   ros::NodeHandle &_nh;
+  ros::Time _loopTimer;
+  ros::Time _startTrackingTimer;
+  ros::Time _resetTimer;
+  ros::Time _detectionTimer;
   ros::Publisher _ballPub;
   tf2_ros::Buffer _tfBuffer;
   tf2_ros::TransformListener *_tfListener;
+  tf2::Vector3 _firstObjPose;
+  Pose _lastObjPose;
+  image_transport::ImageTransport *_it;
   CameraInfoConstPtr _cameraInfo;
   PinholeCameraModel _cameraModel;
-  image_transport::ImageTransport *_it;
-  
+
   cv::Mat _frame;
   cv::Mat _depth;
-  cv::Point3d _ballPos;
-  cv::Point3d startPoint;
-  
+
+  void drawObjPose(Pose &p);
+  void drawObjPredictedLine(std::list<geometry_msgs::Point> &list);
+  void drawObjRealLine(std::list<geometry_msgs::Point> &list);
+  void pubMarker(Marker m);
+  void transformPose(Pose &pose);
+  void conceptOne(cv::Mat &mask, cv::Point2i &center, uint16_t &radius);
+  void conceptTwo(cv::Mat &mask, cv::Point2i &center, uint16_t &radius);
+  float getDistToObj(cv::Mat &mask, uint16_t &radius);
+
+  /**
+   * Builds a 3D point of object from 2D Image via camera model
+   *
+   * @param[in] point2d cv::Point2i - 2d point
+   * @param[in] distToObj distance to interest object
+   * @param[out] point3d geometry_msgs::Pose - 3d point
+   **/
+  void getObjPoseFromCameraModel(cv::Point2i &point2d, float distToObj, Pose &objPose);
+
 public:
-  StateTracking(BallTrackingRos &context, ros::NodeHandle &nh): State(context, "Tracking"), _nh(nh) {}
+  StateTracking(BallTrackingRos &context, ros::NodeHandle &nh) : State(context, "Tracking"), _nh(nh) {}
   ~StateTracking();
 
   bool loadParam();
   bool setup();
- 
-  void drawBallPos(geometry_msgs::Pose p);
-  void drawBallDiract(geometry_msgs::Pose p);
-  void drawPredicted(std::list<geometry_msgs::Point> list);
-  void draBallTrajectory(std::list<cv::Point3d> &bt);
-  void pubMarker(Marker m);
 
   void tracking() override {}
   void wait() override;
--- src/PxSitl/src/vision/StateTracking.cpp
+++ src/PxSitl/src/vision/StateTracking.cpp
@@ -63,26 +63,26 @@ bool StateTracking::setup() {
   if (_tfListener == nullptr)
     _tfListener = new tf2_ros::TransformListener(_tfBuffer, _nh);
 
-  if(_ballPub.getTopic().empty()) {
+  if (_ballPub.getTopic().empty()) {
     _ballPub = _nh.advertise<Marker>("ball", 5, false);
   }
 
   cv::namedWindow(_winName, cv::WINDOW_AUTOSIZE);
   cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
 
-  _timer = ros::Time::now();
-  _lastDetection = ros::Time::now();
-  resetTimer = ros::Time::now();
+  _loopTimer = ros::Time::now();
+  _detectionTimer = ros::Time::now();
+  _resetTimer = ros::Time::now();
 
   return true;
 }
 
-void StateTracking::drawBallPos(geometry_msgs::Pose p) {
+void StateTracking::drawObjPose(Pose &p) {
   Marker m;
 
   m.header.frame_id = "map";
   m.header.stamp = ros::Time::now();
-  m.ns = "ball_pos";
+  m.ns = "obj_pose";
   m.id = 0;
 
   m.pose = p;
@@ -105,18 +105,17 @@ void StateTracking::drawBallPos(geometry_msgs::Pose p) {
   pubMarker(m);
 }
 
-void StateTracking::drawPredicted(std::list<geometry_msgs::Point> list) {
+void StateTracking::drawObjPredictedLine(std::list<geometry_msgs::Point> &list) {
   Marker m;
 
   m.header.frame_id = "map";
   m.header.stamp = ros::Time::now();
-  m.ns = "ball_pred";
+  m.ns = "obj_predicted_line";
   m.id = 1;
   m.action = Marker::MODIFY;
 
-  for (auto &&p : list) {
+  for (auto &&p : list)
     m.points.push_back(p);
-  }
 
   m.pose.orientation.w = 1;
 
@@ -138,42 +137,14 @@ void StateTracking::drawPredicted(std::list<geometry_msgs::Point> list) {
   pubMarker(m);
 }
 
-void StateTracking::drawBallDiract(geometry_msgs::Pose pose) {
-  Marker m;
-
-  m.header.frame_id = "map";
-  m.header.stamp = ros::Time::now();
-  m.ns = "ball_diract";
-  m.id = 2;
-
-  m.pose = pose;
-
-  geometry_msgs::Vector3 scale;
-  scale.x = .3;
-  scale.y = .05;
-  scale.z = .05;
-  m.scale = scale;
-
-  std_msgs::ColorRGBA c;
-  c.a = 1;
-  c.b = 0;
-  c.g = 0;
-  c.r = 1;
-  m.color = c;
-
-  m.type = Marker::ARROW;
-
-  pubMarker(m);
-}
-
-void StateTracking::draBallTrajectory(std::list<cv::Point3d> &bt) {
+void StateTracking::drawObjRealLine(std::list<geometry_msgs::Point> &list) {
   Marker line;
 
   line.header.frame_id = "map";
   line.header.stamp = ros::Time::now();
 
   line.id = 3;
-  line.ns = "trajectory";
+  line.ns = "obj_real_line";
   line.type = Marker::LINE_STRIP;
 
   std_msgs::ColorRGBA c;
@@ -190,13 +161,8 @@ void StateTracking::draBallTrajectory(std::list<cv::Point3d> &bt) {
 
   line.scale = scale;
 
-  geometry_msgs::Point point;
-  for (auto &&i : bt) {
-    point.x = i.x;
-    point.y = i.y;
-    point.z = i.z;
-    line.points.push_back(point);
-  }
+  for (auto &&i : list)
+    line.points.push_back(i);
 
   line.pose.orientation.w = 1;
 
@@ -209,6 +175,177 @@ void StateTracking::wait() {
   ROS_INFO("Transition from %s state to Wait state", toString().c_str());
   cv::destroyAllWindows();
   _context.setState(static_cast<State *>(_context.getStateWait()));
+}
+
+float StateTracking::getDistToObj(cv::Mat &mask, uint16_t &radius) {
+  uint16_t deametr = radius + radius;
+  cv::Mat ballDist(cv::Size2i(deametr, deametr), CV_16UC1, cv::Scalar::all(0));
+  _depth.copyTo(ballDist, mask);
+
+  std::map<uint16_t, uint16_t> mapOfDist;
+  for (auto &&d : cv::Mat_<uint16_t>(ballDist)) {
+    if (d == 0)
+      continue;
+    if (mapOfDist.count(d) > 0)
+      mapOfDist.at(d) += 1;
+    else
+      mapOfDist.insert(std::pair<uint16_t, uint16_t>(d, 0));
+  }
+
+  std::multimap<uint16_t, uint16_t> inv;
+  for (auto &&i : mapOfDist)
+    inv.insert(std::pair<uint16_t, uint16_t>(i.second, i.first));
+
+  /* for (auto &&ii : inv)
+    std::cout << ii.first << ": " << ii.second << std::endl; */
+
+  uint16_t distToBall;
+  for (std::multimap<uint16_t, uint16_t>::const_iterator i = inv.cend(); i != inv.cbegin(); i--) {
+    if (i->second <= 100)
+      continue;
+
+    distToBall = i->second;
+    break;
+  }
+
+  return distToBall * 0.001f;
+}
+
+void StateTracking::getObjPoseFromCameraModel(cv::Point2i &point2d, float distToObj, Pose &objPose) {
+  cv::Point3d point3d = _cameraModel.projectPixelTo3dRay(point2d);
+  objPose.position.x = point3d.x;
+  objPose.position.y = point3d.y;
+  objPose.position.z = distToObj;
+}
+
+void StateTracking::transformPose(Pose &pose) {
+  tf2::Vector3 vectorFromPose;
+  tf2::fromMsg(pose.position, vectorFromPose);
+  tf2::Transform transformFromPose(tf2::Quaternion::getIdentity(), vectorFromPose);
+  tf2::Stamped<tf2::Transform> ts4Transform(transformFromPose, ros::Time::now(), "map");
+  TransformStamped ts(tf2::toMsg(ts4Transform));
+  ts.child_frame_id = _cameraModel.tfFrame();
+
+  TransformStamped transform = _tfBuffer.lookupTransform("map", _cameraModel.tfFrame(), ros::Time(0));
+  TransformStamped newTransform;
+  tf2::doTransform(ts, newTransform, transform);
+
+  pose.orientation = newTransform.transform.rotation;
+  pose.position.x = newTransform.transform.translation.x;
+  pose.position.y = newTransform.transform.translation.y;
+  pose.position.z = newTransform.transform.translation.z;
+}
+
+void StateTracking::conceptOne(cv::Mat &mask, cv::Point2i &center, uint16_t &radius) {
+  try {
+    float distToObj = getDistToObj(mask, radius);
+    Pose newObjPose;
+    getObjPoseFromCameraModel(center, distToObj, newObjPose);
+    transformPose(newObjPose);
+
+    if (_lastObjPose == Pose()) {
+      _lastObjPose = newObjPose;
+      tf2::fromMsg(newObjPose.position, _firstObjPose);
+      _startTrackingTimer = ros::Time::now();
+      return;
+    }
+
+    tf2::Vector3 vecLastObjPosition;
+    tf2::Vector3 vecNewObjPosition;
+    tf2::fromMsg(_lastObjPose.position, vecLastObjPosition);
+    tf2::fromMsg(newObjPose.position, vecNewObjPosition);
+
+    // double dt = 0.01; ///< 100Hz
+    ros::Rate rate(250);
+    if (ros::Time::now() - _detectionTimer >= ros::Duration(rate)) {
+      // ROS_INFO("New timer cycle");
+      // double velocity = dist / ros::Duration(0.1).toSec();
+      // float dist = tf2::tf2Distance2(vecNewObjPosition, vecLastObjPosition);
+
+      // double vX = objDirection.x() / ros::Duration(dt).toSec();
+      // double vY = objDirection.y() / ros::Duration(dt).toSec();
+      // double vZ = objDirection.z() / ros::Duration(dt).toSec();
+
+      // ROS_INFO("vX: %lf, xY: %lf vZ: %lf", vX, vY, vZ);
+
+      // ROS_INFO("objDirection.len = %f", objDirection.length());
+      // ROS_INFO("objDirection.len2 = %f", objDirection.length2());
+      tf2::Vector3 objDirection = vecLastObjPosition - vecNewObjPosition;
+      // ROS_INFO("objDirection x %f; y %f; z %f;", objDirection.x(), objDirection.y(), objDirection.z());
+      if (objDirection.length2() != 0) {
+        tf2::Vector3 totalLength = vecNewObjPosition - _firstObjPose;
+        ros::Duration totalTime = ros::Time::now() - _startTrackingTimer;
+
+        double v = totalLength.length() / totalTime.toSec(); ///< velocity m/s
+        double vX = totalLength.x() / totalTime.toSec();     ///< x axsis velocity
+        double vY = totalLength.y() / totalTime.toSec();     ///< y axsis velocity
+        double vZ = totalLength.z() / totalTime.toSec();     ///< z axsis velocity
+
+        // ROS_INFO("Speed: %f", v);
+        // ROS_INFO("Len for speed: %f", totalLength.length());
+        // ROS_INFO("vecNewObjPosition x %f; y %f; z %f;", vecNewObjPosition.x(), vecNewObjPosition.y(), vecNewObjPosition.z());
+        // ROS_INFO("vecLastObjPosition x %f; y %f; z %f;", vecLastObjPosition.x(), vecLastObjPosition.y(), vecLastObjPosition.z());
+        // double angle = 0.05; ///< angle in radian
+        double angle = asin(totalLength.z() / objDirection.length()); ///< angle in radian
+        // double angle = _medianFilter.filtered(tf2Degrees(asin(objDirection.z() / objDirection.length())));
+        // ROS_INFO("Angle: %f", angle);
+
+        tf2::Vector3 tmpVecObjPos(vecNewObjPosition);
+        tf2::Vector3 tmpVecLastObjPos(vecLastObjPosition);
+        double timePred = 0.1;
+
+        for (size_t i = 0; i < 5; i++) {
+          tf2::Vector3 tmpDirection = tmpVecLastObjPos - tmpVecObjPos;
+          // ROS_INFO("tmpDirection x %f; y %f; z %f;", tmpDirection.x(), tmpDirection.y(), tmpDirection.z());
+          // ROS_INFO("tmpDirection.len: %f", tmpDirection.length());
+          // angle = _medianFilter.filtered(tf2Degrees(asin(tmpDirection.z() / tmpDirection.length())));
+          // angle = asin(tmpDirection.z() / tmpDirection.length()); ///< in radian
+          // ROS_INFO("angle2: %f", angle);
+
+          double dx = vX * timePred;
+          double gt = (9.8f * powl(timePred, 2)) / 2.0f;
+          double dy = (vZ * timePred) - gt;
+
+          // ROS_INFO("dx %f, dy %f", dx, dy);
+
+          tmpVecLastObjPos = tmpVecObjPos;
+          tmpVecObjPos.setX(tmpVecObjPos.getX() + dx);
+          tmpVecObjPos.setZ(tmpVecObjPos.getZ() + dy);
+
+          geometry_msgs::Point p;
+          tf2::toMsg(tmpVecObjPos, p);
+          if (_objPredictedLine.size() > 50)
+            _objPredictedLine.pop_front();
+          _objPredictedLine.push_back(p);
+        }
+        // fixTragectory(_ballPredictedTrajs);
+
+        drawObjPredictedLine(_objPredictedLine);
+      }
+      _objRealLine.push_back(newObjPose.position);
+      _detectionTimer = ros::Time::now();
+    }
+    drawObjRealLine(_objRealLine);
+    // ROS_INFO("Ball move %f", dist);
+
+    Pose pose;
+    tf2::Quaternion q = tf2::shortestArcQuatNormalize2(vecLastObjPosition, vecNewObjPosition);
+    tf2::toMsg(tf2::Transform(q, vecNewObjPosition), pose);
+    drawObjPose(pose);
+    
+    _lastObjPose = newObjPose;
+  } catch (const cv::Exception &e) {
+    ROS_ERROR("[BallTrackingRos] %s", e.what());
+    ros::Duration(1.0).sleep();
+    wait();
+  } catch (const tf2::TransformException &e) {
+    ROS_ERROR("[BallTrackingRos] %s", e.what());
+    ros::Duration(1.0).sleep();
+    wait();
+  }
+}
+
+void StateTracking::conceptTwo(cv::Mat &mask, cv::Point2i &center, uint16_t &radius) {
 }
 
 void StateTracking::execute() {
@@ -230,200 +367,8 @@ void StateTracking::execute() {
   // ROS_DEBUG("RADIUS: %d", radius);
 
   if (radius != 0) {
-    std::stringstream info;
 
-    try {
-      uint16_t deametr = radius + radius;
-      cv::Mat ballDist(cv::Size2i(deametr, deametr), CV_16UC1, cv::Scalar::all(0));
-      _depth.copyTo(ballDist, mask);
-
-      std::map<uint16_t, uint16_t> mapOfDist;
-      for (auto &&d : cv::Mat_<uint16_t>(ballDist)) {
-        if (d == 0)
-          continue;
-        if (mapOfDist.count(d) > 0)
-          mapOfDist.at(d) += 1;
-        else
-          mapOfDist.insert(std::pair<uint16_t, uint16_t>(d, 0));
-      }
-
-      std::multimap<uint16_t, uint16_t> inv;
-      for (auto &&i : mapOfDist)
-        inv.insert(std::pair<uint16_t, uint16_t>(i.second, i.first));
-
-      /* for (auto &&ii : inv)
-        std::cout << ii.first << ": " << ii.second << std::endl; */
-
-      uint16_t distToBall;
-      for (std::multimap<uint16_t, uint16_t>::const_iterator i = inv.cend(); i != inv.cbegin(); i--) {
-        if (i->second <= 100)
-          continue;
-
-        distToBall = i->second;
-        break;
-      }
-
-      info << "CENTER: " << center << " RADIUS: " << radius;
-      cv::Point2f textPos = {center.x + radius + 15.0f, center.y - 15.0f};
-
-      info << " DIST: " << distToBall * 0.001f;
-
-      cv::Point3d newBallPos = _cameraModel.projectPixelTo3dRay(center);
-      newBallPos.z = distToBall * 0.001f;
-
-      tf2::Transform trPoint(tf2::Quaternion::getIdentity(), tf2::Vector3(newBallPos.x, newBallPos.y, newBallPos.z));
-      tf2::Stamped<tf2::Transform> newPointTs(trPoint, ros::Time::now(), "map");
-      geometry_msgs::TransformStamped ts = tf2::toMsg(newPointTs);
-      ts.child_frame_id = _cameraModel.tfFrame();
-
-      TransformStamped transform;
-
-      try {
-        transform = _tfBuffer.lookupTransform("map", _cameraModel.tfFrame(), ros::Time(0));
-      } catch (const tf2::TransformException &e) {
-        ROS_ERROR("[BallTrackingRos] %s", e.what());
-        ros::Duration(1.0).sleep();
-        wait();
-      }
-
-      geometry_msgs::TransformStamped newTs;
-      tf2::doTransform(ts, newTs, transform);
-      newBallPos.x = newTs.transform.translation.x;
-      newBallPos.y = newTs.transform.translation.y;
-      newBallPos.z = newTs.transform.translation.z;
-
-      if (_ballTragectory.size() > 10)
-        _ballTragectory.pop_front();
-
-      _ballTragectory.push_back(newBallPos);
-
-      if (_ballPos == cv::Point3d(0, 0, 0)) {
-        _ballPos = cv::Point3d(newBallPos.x, newBallPos.y, newBallPos.z);
-        startPoint = _ballPos;
-        startTime = ros::Time::now();
-        return;
-      }
-
-      tf2::Vector3 ballPoseV(_ballPos.x, _ballPos.y, _ballPos.z);
-      tf2::Vector3 newBallPoseV(newBallPos.x, newBallPos.y, newBallPos.z);
-
-      double dt = 0.01; // 12.5hz
-
-      if (ros::Time::now() - _lastDetection >= ros::Duration(dt)) {
-        // ROS_INFO("New timer cycle");
-        // double velocity = dist / ros::Duration(0.1).toSec();
-        // float dist = tf2::tf2Distance2(newBallPoseV, ballPoseV);
-
-        // double vX = ballDirV.x() / ros::Duration(dt).toSec();
-        // double vY = ballDirV.y() / ros::Duration(dt).toSec();
-        // double vZ = ballDirV.z() / ros::Duration(dt).toSec();
-
-        // ROS_INFO("vX: %lf, xY: %lf vZ: %lf", vX, vY, vZ);
-
-        // ROS_INFO("ballDirV.len = %f", ballDirV.length());
-        // ROS_INFO("ballDirV.len2 = %f", ballDirV.length2());
-        tf2::Vector3 ballDirV = ballPoseV - newBallPoseV;
-        // ROS_INFO("ballDirV x %f; y %f; z %f;", ballDirV.x(), ballDirV.y(), ballDirV.z());
-        if (ballDirV.length2() != 0) {
-          tf2::Vector3 totalLength = newBallPoseV - tf2::Vector3(startPoint.x, startPoint.y, startPoint.z);
-          ros::Duration totalTime = ros::Time::now() - startTime;
-
-          double v = totalLength.length() / totalTime.toSec(); // speed m/s
-          double vX = totalLength.x() / totalTime.toSec();
-          double vY = totalLength.y() / totalTime.toSec();
-          double vZ = totalLength.z() / totalTime.toSec();
-
-          // ROS_INFO("Speed: %f", v);
-          // ROS_INFO("Len for speed: %f", totalLength.length());
-          // ROS_INFO("newBallPoseV x %f; y %f; z %f;", newBallPoseV.x(), newBallPoseV.y(), newBallPoseV.z());
-          // ROS_INFO("ballPoseV x %f; y %f; z %f;", ballPoseV.x(), ballPoseV.y(), ballPoseV.z());
-          // double angle = 0.05; ///< angle in radian
-          double angle = asin(totalLength.z() / ballDirV.length()); ///< angle in radian
-          // double angle = _medianFilter.filtered(tf2Degrees(asin(ballDirV.z() / ballDirV.length())));
-          // ROS_INFO("Angle: %f", angle);
-
-          tf2::Vector3 tt(newBallPoseV);
-          tf2::Vector3 ptt(ballPoseV);
-
-          std::list<geometry_msgs::Point> trajectory;
-          for (size_t i = 0; i < 5; i++) {
-            tf2::Vector3 pDir = ptt - tt;
-            // ROS_INFO("pDir x %f; y %f; z %f;", pDir.x(), pDir.y(), pDir.z());
-            // ROS_INFO("pDir.len: %f", pDir.length());
-            // angle = _medianFilter.filtered(tf2Degrees(asin(pDir.z() / pDir.length())));
-            // angle = asin(pDir.z() / pDir.length()); ///< in radian
-            // ROS_INFO("angle2: %f", angle);
-
-            double timePred = .1;
-
-            double dx = vX * ros::Duration(timePred).toSec();
-            double gt = (9.8 * powl(ros::Duration(timePred).toSec(), 2)) / 2;
-            double dy = (vZ * ros::Duration(timePred).toSec()) - gt;
-
-            // ROS_INFO("dx %f, dy %f", dx, dy);
-
-            ptt = tt;
-            tt.setX(tt.getX() + dx);
-            tt.setZ(tt.getZ() + dy);
-
-            geometry_msgs::Point p;
-            p.x = tt.x();
-            p.y = tt.y();
-            p.z = tt.z();
-
-            // trajectory.push_back(p);
-            _ballPredictedTraj.push_back(p);
-            // if (_ballPredictedTraj.size() > 50)
-            //   _ballPredictedTraj.pop_front();
-          }
-          // fixTragectory(_ballPredictedTrajs);
-
-          drawPredicted(_ballPredictedTraj);
-        }
-        _lastDetection = ros::Time::now();
-      }
-      // ROS_INFO("Ball move %f", dist);
-
-      tf2::Quaternion q = tf2::shortestArcQuatNormalize2(ballPoseV, newBallPoseV);
-
-      // tf2::Quaternion q;
-      // q.setEulerZYX(ballDirV.z(), ballDirV.y(), ballDirV.x());
-
-      // Utils::fastFilterCvPoint3d(_ballPos, newBallPos, _filterGain);
-
-      geometry_msgs::Pose pose;
-      pose.position.x = newBallPos.x;
-      pose.position.y = newBallPos.y;
-      pose.position.z = newBallPos.z;
-      pose.orientation.w = 1;
-
-      drawBallPos(pose);
-
-      pose.orientation.w = q.w();
-      pose.orientation.x = q.x();
-      pose.orientation.y = q.y();
-      pose.orientation.z = q.z();
-
-      // _context->drawBallDiract(pose);
-
-      draBallTrajectory(_ballTragectory);
-      _ballPos = cv::Point3d(newBallPos.x, newBallPos.y, newBallPos.z);
-
-      // std::stringstream info2;
-      // info2 << _ballPos;
-      // ROS_INFO("Pixel in 3d: %s", info2.str().c_str());
-    } catch (const cv::Exception &e) {
-      std::cerr << e.what() << '\n';
-      wait();
-    }
-
-    /* if (ros::Duration(ros::Time::now() - _timer) <= ros::Duration(1.0)) {
-      framesCount++;
-    } else {
-      fps = framesCount;
-      _timer = ros::Time::now();
-      framesCount = 0;
-    } */
+    conceptOne(mask, center, radius);
 
     cv::circle(tmpFrame, center, radius + 7.0, cv::Scalar::all(128), 1,
                cv::LINE_4);
@@ -431,21 +376,22 @@ void StateTracking::execute() {
                cv::LINE_8);
 
     _fps = static_cast<int>(
-        std::ceil(1.0 / ros::Duration(ros::Time::now() - _timer).toSec()));
+        std::ceil(1.0 / ros::Duration(ros::Time::now() - _loopTimer).toSec()));
 
     cv::Size textSize = cv::getTextSize(
         std::to_string(_fps), cv::FONT_HERSHEY_SIMPLEX, 0.4, 2, nullptr);
     cv::putText(tmpFrame, std::to_string(_fps), cv::Point(5, textSize.height * 2),
                 cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar::all(0));
-    cv::putText(tmpFrame, info.str(),
+    /* cv::putText(tmpFrame, info.str(),
                 cv::Point(5, (textSize.height * 4) + textSize.height),
-                cv::FONT_HERSHEY_SIMPLEX, .4, cv::Scalar::all(0));
+                cv::FONT_HERSHEY_SIMPLEX, .4, cv::Scalar::all(0)); */
   } else {
-
-    if (ros::Time::now() - resetTimer >= ros::Duration(1.0)) {
-      _ballPos = cv::Point3d(0, 0, 0);
-      resetTimer = ros::Time::now();
-      _ballPredictedTraj.clear();
+    if (ros::Time::now() - _resetTimer >= ros::Duration(3.0, 0) && _lastObjPose != Pose()) {
+      _lastObjPose = Pose();
+      _resetTimer = ros::Time::now();
+      _objPredictedLine.clear();
+      _objRealLine.clear();
+      ROS_DEBUG("Clean lines");
     }
   }
 
@@ -458,5 +404,5 @@ void StateTracking::execute() {
     wait();
   }
 
-  _timer = ros::Time::now();
+  _loopTimer = ros::Time::now();
 }
